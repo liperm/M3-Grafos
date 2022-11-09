111f4114dafc8ff1b4067f6079de91efb3f48fa0
Felipe Ribeiro <felipe.ribeiro@hellosaks.com>
2022-11-09 13:43:47 -0300

    mudanças nos campos da tarefa e no print da tabela

--- grafos03.py
+++ grafos03.py
@@ -1,41 +1,52 @@
 import numpy as np
 
 class Tarefa:
-    def __init__(self, id, duracao, precedentes):
-        self.id = id
+    def __init__(self, nome, duracao, precedentes):
+        self.nome = nome
         self.duracao = duracao
         self.precedentes = precedentes
+        self.id = 0
+    
+    def print(self):
+        print(f'id: {self.id} nome: {self.nome} duracao: {self.duracao} precendentes: {self.precedentes}')
 
 class Tabela():
-    def __init__(self, tarefas = []):
-        self.tabelaTarefas = np.array(tarefas)
+    def __init__(self):
+        self.tabelaTarefas = []
+        self.id = 0
 
     def adicionaTarefa(self, tarefa):
-        self.tabelaTarefas = np.append(self.tabelaTarefas, [tarefa])
+        tarefa.id = self.id
+        self.tabelaTarefas.append(tarefa)
+        self.id += 1
 
     def print(self):
-        print(self.tabelaTarefas)
+        for i in self.tabelaTarefas:
+            i.print()
 
 def criarTarefa():
-    id = input('Nome da tarefa: ')
+    nome = input('Nome da tarefa: ')
     duracao = int(input('Duração da tarefa: '))
     precedentes = input('Precedentes: ')
         
     precedentes = list(precedentes)
 
     if(len(id) != 0 and duracao is not None):
-        tarefa = Tarefa(id, duracao, precedentes)
+        tarefa = Tarefa(nome, duracao, precedentes)
         return tarefa
         
     else:
-        raise ValueError('Id ou duracao vazio')
+        raise ValueError('Nome ou duracao vazio')
 
 try:
-    tarefa = criarTarefa()
-
-    tabela = Tabela()
-    tabela.adicionaTarefa(tarefa)
-    tabela.print()
+    t1 = Tarefa('a', 2, [])
+    t2 = Tarefa('b', 3, ['a'])
+    t3 = Tarefa('c', 4, ['a','b'])
+    t = Tabela()
+    t.adicionaTarefa(t1)
+    t.adicionaTarefa(t2)
+    t.adicionaTarefa(t3)
+    t.print()
 
 except ValueError as error:
     print(error)
